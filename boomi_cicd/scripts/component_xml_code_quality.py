621c277f37d04e1069c4170ea87cb92106e138a3
DarkoMirchevski <61994254+DarkoMirchevski@users.noreply.github.com>
2024-12-06 10:56:52 +0100

    Update component_xml_code_quality.py

--- boomi_cicd/scripts/component_xml_code_quality.py
+++ boomi_cicd/scripts/component_xml_code_quality.py
@@ -117,3 +117,8 @@ f.close()
 
 commit_and_push (repo)
 
+with open(f"{base_folder}/report.md", "r") as f:
+    report_content = f.read()
+    if "MAJOR" in report_content:
+        logger.error("Report contains entries with priority 'MAJOR'. Failing the script.")
+        sys.exit(1)  # Exit with a non-zero status to indicate failure
