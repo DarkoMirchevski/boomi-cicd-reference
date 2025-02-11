b65c965f91d33dc610ef7c4091cfaa203cc6c9cc
DarkoMirchevski <61994254+DarkoMirchevski@users.noreply.github.com>
2025-02-11 14:09:38 +0100

    Update component_xml_git.py

--- boomi_cicd/scripts/component_xml_git.py
+++ boomi_cicd/scripts/component_xml_git.py
@@ -3,7 +3,19 @@ import boomi_cicd
 
 # Open release json
 releases = boomi_cicd.set_release()
+#Added Start TODO
+for release in releases["pipelines"]:
+    process_name = release["processName"]
+    component_id = release["componentId"]
+    automated_test_component_id = release["automatedTestId"]
+    package_version = release["packageVersion"]
+    notes = release.get("notes")
 
+# If successful the package and deploy the main process
+        package_id = boomi_cicd.create_packaged_component(
+            component_id, package_version, notes
+        )
+#Added End TODO    
 # Clone repo
 repo = boomi_cicd.clone_repository()
 # Get parent folder name and component ID
