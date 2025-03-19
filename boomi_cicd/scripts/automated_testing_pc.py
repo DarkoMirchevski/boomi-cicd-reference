c56b4a742536b20e5d7bff75e33c6efabb8f45cd
DarkoMirchevski <61994254+DarkoMirchevski@users.noreply.github.com>
2025-03-19 15:06:58 +0100

    Update automated_testing_pc.py

--- boomi_cicd/scripts/automated_testing_pc.py
+++ boomi_cicd/scripts/automated_testing_pc.py
@@ -1,44 +1,44 @@
-
 import boomi_cicd
 import uuid
-
 
 # Open release json
 releases = boomi_cicd.set_release()
 
-environment_id = boomi_cicd.query_environment(boomi_cicd.ENVIRONMENT_NAME)
+environment_name = boomi_cicd.ENVIRONMENT_NAME
+environment_id = boomi_cicd.query_environment(environment_name)
 atom_id = boomi_cicd.query_atom(boomi_cicd.ATOM_NAME)
 
 for release in releases["pipelines"]:
-    automated_test_component_id = release["automatedTestId"]
-    package_version = str(uuid.uuid4())
-    notes = "Automated Test"
     package_id = release.get("packageId")
 
-    automated_test_package_id = boomi_cicd.create_packaged_component(
-        automated_test_component_id, package_version, notes
-    )
+    if environment_name.lower() == "development":
+        automated_test_component_id = release["automatedTestId"]
+        package_version = str(uuid.uuid4())
+        notes = "Automated Test"
 
-    # Deploy the automated test harness
-    automated_test_deploymentId = boomi_cicd.create_deployed_package(
-        release, automated_test_package_id, environment_id
-    )
-
-    # Execute the test process
-    request_id = boomi_cicd.create_execution_request(
-        atom_id, automated_test_component_id
-    )
-
-    # Check the completed status. Anything other than COMPLETE means it did not pass.
-    execution_response = boomi_cicd.get_completed_execution_status(request_id)
-
-    # Raise an exception if the status of the execution is anything other than COMPLETE
-    if execution_response["status"] != "COMPLETE":
-        raise AssertionError(
-            f"Automation test failed. Error message: {execution_response['message']}"
+        automated_test_package_id = boomi_cicd.create_packaged_component(
+            automated_test_component_id, package_version, notes
         )
-    boomi_cicd.delete_deployed_package(automated_test_deploymentId)
-    
-    boomi_cicd.create_deployed_package(release, package_id, environment_id)
 
-    
+        # Deploy the automated test harness
+        automated_test_deploymentId = boomi_cicd.create_deployed_package(
+            release, automated_test_package_id, environment_id
+        )
+
+        # Execute the test process
+        request_id = boomi_cicd.create_execution_request(
+            atom_id, automated_test_component_id
+        )
+
+        # Check the completed status. Anything other than COMPLETE means it did not pass.
+        execution_response = boomi_cicd.get_completed_execution_status(request_id)
+
+        # Raise an exception if the status of the execution is anything other than COMPLETE
+        if execution_response["status"] != "COMPLETE":
+            raise AssertionError(
+                f"Automation test failed. Error message: {execution_response['message']}"
+            )
+        boomi_cicd.delete_deployed_package(automated_test_deploymentId)
+
+    # Always deploy the actual package, regardless of environment
+    boomi_cicd.create_deployed_package(release, package_id, environment_id)
