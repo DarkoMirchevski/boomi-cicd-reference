5aa043f1c5e3e2835018ada8cd710b313f0fa07c
DarkoMirchevski <61994254+DarkoMirchevski@users.noreply.github.com>
2025-04-04 14:04:51 +0200

    Update component_xml_git_packaged_component.py

--- boomi_cicd/scripts/component_xml_git_packaged_component.py
+++ boomi_cicd/scripts/component_xml_git_packaged_component.py
@@ -1,6 +1,6 @@
 import logging
 import boomi_cicd
-
+import xml.etree.ElementTree as ET
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
@@ -30,3 +30,38 @@ print(f"Component ID: {component_id}")
 
 componentxml = boomi_cicd.query_component(component_id)
 print(f"Compnent XML: {componentxml}")
+
+
+# Clone repo
+#repo = boomi_cicd.clone_repository()
+
+# Get parent folder name and component ID mapping
+#file_components = boomi_cicd.get_component_xml_file_refs(boomi_cicd.COMPONENT_REPO_NAME)
+
+
+# Process each release to update the Git repository with the component XMLs
+for release in releases["pipelines"]:
+    print("packageVersion:", package.get("packageVersion"))
+    release["packageVersion"] = package.get("packageVersion")
+
+    print("componentId:", package.get("componentId"))
+    release["componentId"] = package.get("componentId")
+
+    # Parse the XML string
+    root = ET.fromstring(component_xml)
+    
+    # Find the 'name' attribute in the component XML
+    process_name = root.attrib.get('name')
+    print("Process name:", process_name)
+
+    release["processName"] = release["processName"]
+
+    print("packageVersion:", package.get("packageVersion"))
+    release["packageVersion"] = package.get("packageVersion")
+    #boomi_cicd.process_git_release(repo, file_components, release)
+
+# Save the updated component references
+#boomi_cicd.set_component_xml_file_refs(boomi_cicd.COMPONENT_REPO_NAME, file_components)
+
+# Commit and push changes
+#boomi_cicd.commit_and_push(repo)
