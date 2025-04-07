809a7fc208ed320243e2d03553c1b133b7f472ea
DarkoMirchevski <61994254+DarkoMirchevski@users.noreply.github.com>
2025-04-07 13:45:03 +0200

    Update component_xml_git_packaged_component.py

--- boomi_cicd/scripts/component_xml_git_packaged_component.py
+++ boomi_cicd/scripts/component_xml_git_packaged_component.py
@@ -6,30 +6,10 @@ import xml.etree.ElementTree as ET
 logging.basicConfig(level=logging.DEBUG)
 
 
-def get_packaged_component(packaged_component_id):
-    """
-    Get a packaged component.
-
-    :param packaged_component_id: The ID of the packaged component.
-    :type packaged_component_id: str
-    :return: The packaged component details.
-    :rtype: dict
-    """
-    resource_path = f"/PackagedComponent/{packaged_component_id}"
-    response = boomi_cicd.requests_get(resource_path)
-    return response.json()
-
-packaged_component_id = "c52b35c5-3a74-4e67-88b9-ba80c1ff840f"
-package = get_packaged_component(packaged_component_id)
-
-logging.info(f"Retrieved package: {package}")
-
+package = boomi_cicd.get_packaged_component(packaged_component_id)
 component_id = package.get("componentId")
-print(f"Component ID: {component_id}")
-
-
 componentxml = boomi_cicd.query_component(component_id)
-print(f"Compnent XML: {componentxml}")
+
 
 
 # Clone repo
@@ -43,10 +23,10 @@ releases = boomi_cicd.set_release()
 
 # Process each release to update the Git repository with the component XMLs
 for release in releases["pipelines"]:
-    print("packageVersion:", package.get("packageVersion"))
+    package = boomi_cicd.get_packaged_component(release["packageId"])
+    component_id = package.get("componentId")
+    componentxml = boomi_cicd.query_component(component_id)
     release["packageVersion"] = package.get("packageVersion")
-
-    print("componentId:", package.get("componentId"))
     release["componentId"] = package.get("componentId")
 
     # Parse the XML string
@@ -55,12 +35,7 @@ for release in releases["pipelines"]:
     # Find the 'name' attribute in the component XML
     processName = root.attrib.get('name')
     folderFullPath = root.attrib.get('folderFullPath')
-    print("Process name:", processName)
-    print("folderFullPath:", folderFullPath)
-
     release["processName"] = processName
-
-    print("packageVersion:", package.get("packageVersion"))
     release["packageVersion"] = package.get("packageVersion")
     release["folderFullPath"] = folderFullPath
     boomi_cicd.process_git_release(repo, file_components, release)
